f5b74687b0b888281051bd5cba531b801c992c88
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-03-23 18:01:52 +0900

    Completed

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,21 +1,46 @@
 function balancingParentheses(string) {
-  // type your code here
+  const strArray = string.split("");
+  const chars = { "(": 0, ")": 0 };
+
+  if (strArray.length === 1) {
+    return 1;
+  }
+
+  for (let char of strArray) {
+    chars[char]++;
+  }
+
+  const solution = Math.abs(Object.values(chars)[0] - Object.values(chars)[1]);
+
+  return string[0] === ")" && string[strArray.length - 1] === "("
+    ? solution + 2
+    : solution;
 }
+
+/**
+ Split string to array for looping
+ make a hash with keys of ( and )
+ */
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
-  console.log(balancingParentheses('(()())'));
+  console.log(balancingParentheses("(()())"));
 
   console.log("");
 
   console.log("Expecting: 2");
-  console.log(balancingParentheses('()))'));
+  console.log(balancingParentheses("()))"));
 
   console.log("");
 
   console.log("Expecting: 1");
-  console.log(balancingParentheses(')'));
+  console.log(balancingParentheses(")"));
+
+  console.log("");
+
+  console.log("Expecting: 2");
+  console.log(balancingParentheses(")((((()))((())))()(()))("));
 }
 
 module.exports = balancingParentheses;
